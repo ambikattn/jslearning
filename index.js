7bacf7b57044d355bb23edcaa619da7d1e7cb499
ambikattn <ambika.sharma@tothenew.com>
2020-01-30 16:52:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/*Prototype and inhertence example
+Animal is a prototype here, 
+
+*/
 var Animal = function(legs,eyes,age){
     this.legs = legs;
     this.eyes = eyes;
@@ -5,5 +9,5 @@ var Animal = function(legs,eyes,age){
 }
 Animal.prototype.nature = 'nice';
 var rabbit = new Animal('4','2','12');
-console.log(rabbit.legs);
-console.log(rabbit.nature);
+console.log(rabbit.legs);//4
+console.log(rabbit.nature);//nice
